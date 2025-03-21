f4fabc1986cd991071b5d0c7dff20c7cde6aa77c
ssidiropoulos99 <ssidiropoulos@covariance.gr>
2025-03-21 10:19:54 +0200

    Updated

--- Frontend/src/app/app.component.ts
+++ Frontend/src/app/app.component.ts
@@ -1,6 +1,5 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
-import { error } from 'console';
 
 @Component({
   selector: 'app-root',
@@ -14,10 +13,16 @@ export class AppComponent implements OnInit {
 
   private apiURL= "http://localhost:5001/api/todo";
 
-  printAllData= false;
-  printDataByID= false;
-  printPending = false;
-  printCompleted = false;
+  allData: any [] = [];
+  filteredData: any [] = [];
+  deleteFlag = false;
+
+  formData: any= {
+    //ID: null,
+    itemTitle: null,
+    description: null,
+    completed: false,
+  };
 
   constructor (private http: HttpClient) {}
 
@@ -25,75 +30,80 @@ export class AppComponent implements OnInit {
     this.getAll();
   }
 
-  public allData: any [] = [];
-  public getAll() {
-    this.printAllData = true;
-    this.printDataByID = false;
-    this.http.get(this.apiURL).subscribe(
-      (result:any) => {
-        console.log(result);
-        this.allData = result;
-      }
+  onFilter(event: any) {
+    const value = event.target.value;
+    switch(value) {
+    case "showAll": {
+      this.filteredData=this.allData;
+       break;
+    }
+    case "showPending": {
+      this.pendingItem();
+       break;
+    }
+    case "showCompleted": {
+       this.completedItem();
+       break;
+    }
+    case "byId": {
+      this.getById();
+      break;
+    }
+    }
+  }
 
+  getAll() {
+    this.http.get(this.apiURL).subscribe(
+      {next: (result:any) => {this.allData = result; this.filteredData = result;}}
     );
   }
 
   pendingItem() {
-    this.printPending= true;
-    this.printAllData= false;
-    this.printCompleted= false;
+    this.filteredData = this.allData.filter(new_data => new_data.completed == false);
   }
 
   completedItem() {
-    this.printCompleted= true;
-    this.printPending = false;
-    this.printAllData= false;
+    this.filteredData = this.allData.filter(new_data => new_data.completed == true);
   }
 
-  public dataById: any [] = [];
   getById() {
-    this.printAllData = false;
-    this.printDataByID = true;
     let id = this.checkNumber("print");
-
     if (!id) {
-      alert("no such id");
       return ;
     }
-
-    this.http.get(`${this.apiURL}/${id}`).subscribe({
-      next: (result:any) => {this.dataById = [result];}
-      , error: () => {alert("No such ID found.")}}
-    );
+    this.deleteFlag=true;
+    this.filteredData= this.allData.filter(new_data => new_data.id == id);
+    if (this.filteredData.length == 0) {
+      alert("No such ID found. Instead all To-Do list was printed.");
+      this.filteredData = this.allData;
+    }
   }
-
-  formData = {
-    //ID: null,
-    itemTitle: '',
-    description: '',
-    completed: false,
-  };
 
   postNewItem() {
     this.formData.itemTitle = this.checkString("title");
     if (!this.formData.itemTitle) {
-      return ;
+      return;
     }
 
     this.formData.description = this.checkString("description");
-    if (!this.formData.description) {
+    if (this.formData.description === null) {
       return;
     }
+
     this.http.post(this.apiURL, this.formData).subscribe(
         {next:() => {window.location.reload()}}
       );
     }
 
-
   deleteItem(id: number) {
     this.http.delete(`${this.apiURL}/${id}`).subscribe(
-      {next:() => {this.allData = this.allData.filter(new_data => new_data.id !== id);}}
+      {next:() => {this.allData = this.allData.filter(new_data => new_data.id !== id);
+        this.filteredData = this.filteredData.filter(new_data => new_data.id !== id);}}
     );
+
+    if(this.deleteFlag){
+      this.filteredData=this.allData;
+    }
   }
 
   markItem(item: any) {
@@ -108,10 +118,11 @@ export class AppComponent implements OnInit {
 
       if (new_string === null) {
         alert("You have canceled the input.");
-        return "";
+        return null;
       }
+
       //trim() method is used to remove whitespace from both sides of a string
-      if (new_string.trim() != "" || flag === "description") {
+      if (new_string.trim() !== "" || flag === "description") {
         return new_string;
       } else {
         alert(`Please enter a valid ${flag}.`);
